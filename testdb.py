a49246546418cfc8363cdd14d9710fc726c7d3de
Artem Tokar <atokar@ambimetrics.com>
2022-02-24 23:27:44 +0100

    test start dockers db and postgres

--- testdb.py
+++ testdb.py
@@ -1,27 +1,8 @@
 import psycopg2
-import sys
-
-
-con = None
 
 
 conn = psycopg2.connect(
-  dbname="anunciosdb",
-  user="atokar",
-  password="1234"
+  dbname="postgres",
+  user="postgres",
+  password=""
 )
-#     cur = con.cursor()
-#     cur.execute('SELECT 1 from mytable')          
-#     ver = cur.fetchone()
-#     print(ver)
-#
-#
-# except psycopg2.DatabaseError:
-#     print(psycopg2.DatabaseError)   
-#     sys.exit(1)
-#
-#
-# finally:
-#
-#     if con:
-#         con.close()
