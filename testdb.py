4bd8426a422ef265f42a048889287e3b02a407ea
Artem Tokar <atokar@ambimetrics.com>
2022-02-27 23:02:58 +0100

    Testing docker network 27/02/2022

--- testdb.py
+++ testdb.py
@@ -1,5 +1,5 @@
-import request
+import requests
 
 
-page = request.get('http://0.0.0.0:8000/anuncios/default/index')
-print(page.text)
+page = requests.get('http://0.0.0.0:8000/anuncios/default/index')
+print(page[0])
