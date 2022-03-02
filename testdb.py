548a2523f01fc1462e643b1c20a844eb8fb3e677
Artem Tokar <atokar@ambimetrics.com>
2022-03-02 16:04:35 +0100

    test 01032022

--- testdb.py
+++ testdb.py
@@ -1,5 +1,9 @@
 import requests
 
 
-page = requests.get('http://0.0.0.0:1234/anuncios_copy/default/create')
-print(page.text)
+def testDB():
+    page = requests.get('http://0.0.0.0:1234/anuncios_copy/default/create')
+    print(page.text)
+    
+if __name__ == '__main__':
+    testDB()
